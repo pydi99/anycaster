404bed40a278b62b46d4bf6d70c1f64574d952fb
pydi99 <pydithallinaidu2399@gmail.com>
2024-03-25 19:07:11 +0530

    Creating anycaster

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+import Canvas from './../components/canvas'
+export default function Home() {
+  return (
+    <div>
+      <Canvas />
+    </div>
+  )
+}
---
+++ components/canvas.tsx
@@ -0,0 +1,5 @@
+export default function Home() {
+  return (
+    <div>Hey there how are you</div>
+  )
+}
